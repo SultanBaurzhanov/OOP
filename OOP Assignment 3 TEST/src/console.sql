8639bc87bd29006ba1caccd4b7aacbb564f5497a
Arman757 <117672999+Arman757@users.noreply.github.com>
2023-01-30 19:23:03 +0500

    Update console.sql

--- OOP Assignment 3 TEST/src/console.sql
+++ OOP Assignment 3 TEST/src/console.sql
@@ -12,19 +12,25 @@ insert into Aisle
 values
 (1, 'Sweater', 'White', 'M', 'Turtleneck'),
 (2, 'Shirt', 'Grey', 'S', 'Turtleneck'),
-(3, 'Sweater', 'Black', 'XL', 'Styrophome');
+(3, 'Shirt', 'White', 'X', 'Turtleneck')
+(4, 'Trousers', 'Black', 'M', 'Styrophome')
+(5, 'Shorts', 'Blue', 'XL', 'Styrophome')
+(6, 'Coat', 'Black', 'S', 'Turtleneck')
+(7, 'Jacket', 'Brown', 'XXL', 'Styrophome')
+(8, 'Shirt', 'White', 'L', 'Turtleneck')
+(9, 'Cardigan', 'Black', 'L', 'Styrophome')
+(10, 'Shirt', 'Red', 'S', 'Turtleneck')
+(11, 'Shirt', 'Blue', 'X', 'Turtleneck')
+(12, 'Jeans', 'Grey', 'M', 'Styrophome')
+(13, 'Sweater', 'Orange', 'L', 'Turtleneck')
+(14, 'Shirt', 'Red', 'L', 'Turtleneck')
+(15, 'Sweater', 'Black', 'XL', 'Styrophome');
 --здесь все добавляете
 --добавляет в пустую таблицу хар-ки как Вещь - свитер/Цвет - Белый/Размер - М/Тип - с воротником (последовательность важна)
 --кстати нужно таблицу удалять каждый раз когда хотите новые данные внести после того как запустили прогу, тк старые данные наложятся на новые и будут копии (database > postgres@localhost > postgres > public > tables > удалите Aisle и запустите эту и первую функцию)
 --если закончили в конце вместо запятой поставить ;
 
-select * from Aisle;
---выводит в строку всю таблицу
-
-select color, Type_Of_Clothes from Aisle;
---выводит определенный столбец, НЕ СТРОКУ (может сколько угодно столбцов вывести)
-
-select * from Aisle where Type_Of_Clothes = 'Turtleneck';
+select * from Aisle;bhu 'Turtleneck';
 --выводит опр строки с "Turtleneck"
 
 
